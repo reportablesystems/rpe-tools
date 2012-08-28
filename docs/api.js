d2db6704eacda3e66d3acfa1dd83dacaffc2292d
Alex Feseto <alexfeseto@gmail.com>
2012-08-28 15:55:56 +0100

    Add YUIDoc generated HTML.

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Logger",
+        "RPETools",
+        "Storage"
+    ],
+    "modules": [
+        "RPE"
+    ],
+    "allModules": [
+        {
+            "displayName": "RPE",
+            "name": "RPE"
+        }
+    ]
+} };
+});
